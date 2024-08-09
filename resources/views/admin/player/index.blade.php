47554c60eeac73c638d1391679082cf0154505dc
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-09 10:09:58 +0630

    report updated

--- resources/views/admin/player/index.blade.php
+++ resources/views/admin/player/index.blade.php
@@ -92,10 +92,10 @@
                 <i class="fas fa-minus text-white me-1"></i>
                   WDL
                 </a>
-                <a href="{{ route('admin.report.view', $user->user_name) }}" data-bs-toggle="tooltip" data-bs-original-title="Reports" class="btn btn-info btn-sm">
+                {{-- <a href="{{ route('admin.report.view', $user->user_name) }}" data-bs-toggle="tooltip" data-bs-original-title="Reports" class="btn btn-info btn-sm">
                   <i class="fas fa-line-chart text-white me-1"></i>
                   Reports
-                </a>
+                </a> --}}
                 <a href="{{ route('admin.logs', $user->id) }}" data-bs-toggle="tooltip" data-bs-original-title="Reports" class="btn btn-info btn-sm">
                   <i class="fas fa-right-left text-white me-1"></i>
                   Logs

78764f5c472ca537e7bc45df2bd43af14170a4e4
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-12 12:13:55 +0630

    login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\BannerController;
+use App\Http\Controllers\Api\V1\PromotionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +26,11 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/player-change-password', [AuthController::class, 'playerChangePassword']);
 
+Route::get('promotion', [PromotionController::class, 'index']);
+Route::get('banner', [BannerController::class, 'index']);
+Route::get('bannerText', [BannerController::class, 'bannerText']);
+//Route::get('adspopup', [BannerController::class, 'AdsBannerIndex']);
+Route::get('popup-ads-banner', [BannerController::class, 'AdsBannerIndex']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
